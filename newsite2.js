4bc602a3e955c7d3e0263e6d9ce27040e858dfad
Carlos Daniel <78973797+CarlosDaniel0035@users.noreply.github.com>
2022-11-30 15:14:06 -0300

    Add files via upload

---
+++ newsite2.js
@@ -0,0 +1,21 @@
+var nome, twitter, facebook, instagram, linkedin, sexo, polo, ano;
+
+nome = "Carlos Daniel Silva Araujo";
+twitter = "@Daniel.silva7";
+facebook = "@Daniel.silva7";
+instagram = "@Daniel.silva7";
+linkedin = "@Daniel.silva7";
+sexo = "Masculino";
+polo = "Alphaville";
+ano = 2022;
+
+document.write(nome + "<br>");
+document.write("Ano - " + ano + "<br>");
+document.write("Twitter: " + twitter + "<br>");
+document.write("Facebook:" + facebook + "<br>");
+document.write("Instagram: " + instagram + "<br>");
+document.write("Linkedin: " + linkedin + "<br>");
+document.write("Sexo: " + sexo + "<br>");
+document.write("Localização - Polo: " + polo);
+
+
